716f0d5c9e7badb7f1916cb4ebcfa833e05209d3
Brian Frank <58821032+briansfrank@users.noreply.github.com>
2012-09-26 07:31:26 -0400

    Beef up timezone handling

--- haystack/HTimeZone.java
+++ haystack/HTimeZone.java
@@ -35,7 +35,11 @@ public final class HTimeZone
 
       // map haystack id to Java full id
       String javaId = (String)toJava.get(name);
-      if (javaId == null) throw new RuntimeException("Unknown tz: " + name);
+      if (javaId == null)
+      {
+        if (checked) throw new RuntimeException("Unknown tz: " + name);
+        return null;
+      }
 
       // resolve full id to HTimeZone and cache
       TimeZone java = TimeZone.getTimeZone(javaId);
@@ -86,15 +90,46 @@ public final class HTimeZone
   {
     HashMap toJava = new HashMap();
     HashMap fromJava = new HashMap();
-    String[] ids = TimeZone.getAvailableIDs();
-    for (int i=0; i<ids.length; ++i)
+    try
     {
-      String java = ids[i];
-      int slash = java.indexOf('/');
-      if (slash < 0) continue;
-      String haystack = java.substring(slash+1);
-      toJava.put(haystack, java);
-      fromJava.put(java, haystack);
+      // only time zones which start with these
+      // regions are considered valid timezones
+      HashMap regions = new HashMap();
+      regions.put("Africa",     "ok");
+      regions.put("America",    "ok");
+      regions.put("Antarctica", "ok");
+      regions.put("Asia",       "ok");
+      regions.put("Atlantic",   "ok");
+      regions.put("Australia",  "ok");
+      regions.put("Etc",        "ok");
+      regions.put("Europe",     "ok");
+      regions.put("Indian",     "ok");
+      regions.put("Pacific",    "ok");
+
+      // iterate Java timezone IDs available
+      String[] ids = TimeZone.getAvailableIDs();
+      for (int i=0; i<ids.length; ++i)
+      {
+        String java = ids[i];
+
+        // skip ids not formatted as Region/City
+        int slash = java.indexOf('/');
+        if (slash < 0) continue;
+        String region = java.substring(0, slash);
+        if (regions.get(region) == null) continue;
+
+        // get city name as haystack id
+        slash = java.lastIndexOf('/');
+        String haystack = java.substring(slash+1);
+
+        // store mapping b/w Java <-> Haystack
+        toJava.put(haystack, java);
+        fromJava.put(java, haystack);
+      }
+    }
+    catch (Throwable e)
+    {
+      e.printStackTrace();
     }
     HTimeZone.toJava   = toJava;
     HTimeZone.fromJava = fromJava;
@@ -118,13 +153,23 @@ public final class HTimeZone
       e.printStackTrace();
     }
 
-    HTimeZone def;
+    HTimeZone def = null;
     try
     {
-      def = HTimeZone.make(TimeZone.getDefault());
+      // check if configured with system property
+      String defName = System.getProperty("haystack.tz");
+      if (defName != null)
+      {
+        def = HTimeZone.make(defName, false);
+        if (def == null) System.out.println("WARN: invalid haystack.tz system property: " + defName);
+      }
+
+      // if we still don't have a default, try to use Java's
+      if (def == null) def = HTimeZone.make(TimeZone.getDefault());
     }
     catch (Exception e)
     {
+      // fallback to UTC
       e.printStackTrace();
       def = utc;
     }
