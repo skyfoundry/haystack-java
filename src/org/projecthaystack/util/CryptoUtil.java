59cc508d6c4439d3e622f83c09cbb6da3abc616c
Mike Jarmy <mjarmy@gmail.com>
2015-09-08 15:25:11 -0400

    add Password Based Key deriviation to CryptoUtil

--- src/org/projecthaystack/util/CryptoUtil.java
+++ src/org/projecthaystack/util/CryptoUtil.java
@@ -9,6 +9,9 @@
 package org.projecthaystack.util;
 
 import java.security.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+
 
 /**
   * Crypto utilities
@@ -16,7 +19,7 @@ import java.security.*;
 public class CryptoUtil
 {
   /**
-   * Implementation of HMAC algorthm since Java's implementation
+   * Implementation of HMAC algorithm since Java's implementation
    * doesn't allow empty passwords
    */
   public static byte[] hmac(String algorithm, byte[] data, byte[] key)
@@ -67,4 +70,98 @@ public class CryptoUtil
     return md.digest();
   }
 
+  /**
+    * Derive a Password-Based Key.  The only currently supported algorithm
+    * is "PBKDF2WithHmacSHA256".
+    */
+  public static byte[] pbk(
+    String algorithm,
+    byte[] password, 
+    byte[] salt, 
+    int iterationCount,
+    int derivedKeyLength)
+    throws Exception
+  {
+    if (!algorithm.equals("PBKDF2WithHmacSHA256"))
+      throw new IllegalArgumentException("Unsupported algorithm: " + algorithm);
+
+    return Pbkdf2.deriveKey(password, salt, iterationCount, derivedKeyLength);
+  }
+
+  /**
+    * Pbkdf2
+    *
+    * https://tools.ietf.org/html/rfc2898#section-5.2
+    * https://tools.ietf.org/html/rfc5802#section-2.2
+    *
+    * implementation is from
+    * http://stackoverflow.com/questions/9147463/java-pbkdf2-with-hmacsha256-as-the-prf
+    *
+    * test suite is from 
+    * http://stackoverflow.com/questions/5130513/pbkdf2-hmac-sha2-test-vectors/5130543#5130543
+    */
+  static class Pbkdf2
+  {
+    /**
+      * deriveKey
+      */
+    static byte[] deriveKey(
+      byte[] password, 
+      byte[] salt, 
+      int iterations,
+      int dkLen)
+    throws NoSuchAlgorithmException, InvalidKeyException
+    {
+      SecretKeySpec keyspec = new SecretKeySpec(password, "HmacSHA256");
+      Mac prf = Mac.getInstance("HmacSHA256");
+      prf.init(keyspec);
+
+      int hLen = prf.getMacLength(); // 20 for SHA1
+      int l = Math.max(dkLen, hLen); //  1 for 128bit (16-byte) keys
+      int r = dkLen - (l-1)*hLen;    // 16 for 128bit (16-byte) keys
+      byte T[] = new byte[l * hLen];
+      int ti_offset = 0;
+      for (int i = 1; i <= l; i++) {
+        F(T, ti_offset, prf, salt, iterations, i);
+        ti_offset += hLen;
+      }
+
+      if (r < hLen) {
+        // Incomplete last block
+        byte DK[] = new byte[dkLen];
+        System.arraycopy(T, 0, DK, 0, dkLen);
+        return DK;
+      }
+      return T;
+    } 
+
+    static void F(byte[] dest, int offset, Mac prf, byte[] S, int c, int blockIndex) 
+    {
+      final int hLen = prf.getMacLength();
+      byte U_r[] = new byte[ hLen ];
+      byte U_i[] = new byte[S.length + 4];
+      System.arraycopy(S, 0, U_i, 0, S.length);
+      INT(U_i, S.length, blockIndex);
+      for(int i = 0; i < c; i++) {
+        U_i = prf.doFinal(U_i);
+        xor(U_r, U_i);
+      }
+
+      System.arraycopy(U_r, 0, dest, offset, hLen);
+    }
+
+    static void xor(byte[] dest, byte[] src) 
+    {
+      for(int i = 0; i < dest.length; i++)
+        dest[i] ^= src[i];
+    }
+
+    static void INT(byte[] dest, int offset, int i) 
+    {
+      dest[offset + 0] = (byte) (i / (256 * 256 * 256));
+      dest[offset + 1] = (byte) (i / (256 * 256));
+      dest[offset + 2] = (byte) (i / (256));
+      dest[offset + 3] = (byte) (i);
+    } 
+  }
 }
